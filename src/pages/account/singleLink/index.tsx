2f55cacd4aae0431ec9e3a0f646f5029a8d80b57
abel-otegbola <abel15655@gmail.com>
2026-01-11 19:48:29 +0100

    feat: update SingleLinkPage layout and display payment link reference

--- src/pages/account/singleLink/index.tsx
+++ src/pages/account/singleLink/index.tsx
@@ -26,14 +26,14 @@ function SingleLinkPage() {
 
 
     return (
-        <div className="flex md:flex-nowrap flex-wrap gap-4">
+        <div className="flex md:flex-nowrap flex-wrap gap-8">
             <div className="flex flex-col md:w-[65%] w-full 2xl:gap-8 md:gap-6 gap-4 2xl:px-12 md:px-6">
                 <div className="flex items-center justify-between gap-4 flex-wrap py-4 border-b border-gray-500/[0.1]">
                     <h1 className="md:text-[18px] text-[16px] font-medium capitalize leading-[28px]">Payment link</h1>
-                    <p> #{id}</p>
+                    <p> #{paymentLink.reference}</p>
                 </div>
 
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 md:p-6 p-4 rounded-lg bg-primary/[0.08] border border-gray-500/[0.1]">
                     <div className="p-3 rounded-full bg-primary/[0.08]"><CardsIcon size={24} /></div>
                     <div className="flex flex-col">
                         <h2 className="opacity-[0.7] text-[12px]">Amount</h2>
@@ -42,7 +42,7 @@ function SingleLinkPage() {
                 </div>
 
                 {/* Link */}
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2 py-4">
                     <p className="opacity-[0.7]">Payment Link</p>
                     <div className="flex items-center gap-2 mt-1">
                         <Input
@@ -59,11 +59,11 @@ function SingleLinkPage() {
                 </div>
 
                 {/* Basic Info */}
-                <div className="flex flex-col gap-1 md:p-6 p-4 rounded-lg bg-primary/[0.08] border border-gray-500/[0.1]">
+                <div className="flex flex-col gap-1 p-4 border border-gray-500/[0.1] rounded-lg">
                     <p className="font-semibold mb-2">Payment Details</p>
                     <div className="flex items-center justify-between">
                         <p className="opacity-[0.7]">Amount</p>
-                        <p className="text-lg font-semibold mt-1">
+                        <p className="text-sm font-medium mt-1">
                         {formatCurrency(paymentLink.amount, paymentLink.currency)}
                         </p>
                     </div>
